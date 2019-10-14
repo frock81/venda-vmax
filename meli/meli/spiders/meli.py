5724d3265eb4da36d9ef2674294196e43a1f5bd6
Fabio Rocha <frock81@yahoo.com.br>
2019-10-13 21:17:50 -0300

    CSV file

--- meli/meli/spiders/meli.py
+++ meli/meli/spiders/meli.py
@@ -1,4 +1,5 @@
 import scrapy
+import os
 
 class MeliSpider(scrapy.Spider):
     name = 'meli'
@@ -6,14 +7,18 @@ class MeliSpider(scrapy.Spider):
         'https://lista.mercadolivre.com.br/yamaha-vmax-1200-1995#D[A:yamaha%\
         20vmax%201200%201995]'
     ]
+    csv_filename = 'meli.csv'
 
     def parse(self, response):
+        os.remove(self.csv_filename)
+        with open(self.csv_filename, 'w') as csv_file:
+            csv_file.write("mileage;description;title;price;location;year\n".encode('utf-8'))
         for result in response.css('.item__info-link'):
             year, mileage = (x.strip() for x in result.css('.item__attrs::text')
                 .get().split('|'))
-            yield {
+            obj = {
                 'title': result.css('.main-title::text').get().strip(),
-                'value': result.css('.price__fraction::text').get(),
+                'price': result.css('.price__fraction::text').get(),
                 'year': year,
                 'mileage': mileage,
                 'location': result.css('.item__location::text').get()
@@ -21,11 +26,29 @@ class MeliSpider(scrapy.Spider):
             next_page = result.css('a::attr(href)').get()
             if next_page is not None:
                 next_page = response.urljoin(next_page)
-                yield scrapy.Request(next_page, callback=self.parseLink)
+                yield scrapy.Request(next_page, callback=self.parseLink,
+                    cb_kwargs=dict(obj=obj))
 
-    def parseLink(self, response):
-        yield {
-            'description': response.css('#description-includes p::text').get()
-        }
+    def parseLink(self, response, obj):
+        obj['description'] = response.css('#description-includes p::text').get()
+        str = ''
+        objLen = len(obj)
+        i = 1
+        for field in obj:
+            # yield {'field': field, 'i': i, 'value': obj[field]}
+            if obj[field] != None:
+                if i == objLen:
+                    str += "'" + obj[field] + "'\n"
+                else:
+                    str += "'" + obj[field] + "';"
+            else:
+                if i == objLen:
+                    str += "''\n"
+                else:
+                    str += "'';"
+            i += 1
+        with open('meli.csv', 'a') as csv_file:
+            csv_file.write(str.encode('utf-8'))
+        yield obj
 
 
